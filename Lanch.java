2fd8d1464422317babc31b8cf8066d8063e56325
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-04-28 17:10:20 +0530

    Add files via upload

---
+++ Lanch.java
@@ -0,0 +1,22 @@
+package com.Project;
+
+public class Lanch {
+
+	public static void main(String[] args) {
+		int arr[]=new int[5];
+		arr[0]=10;
+		arr[1]=11;
+		arr[2]=12;
+		arr[3]=13;
+		arr[4]=14;
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		
+		
+
+	}
+
+}
