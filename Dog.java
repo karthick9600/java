e529d08cd92a0d41de70a74ae145d66a4204613d
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-05-03 17:13:01 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,38 @@
+package com.Project;
+
+public class Dog {
+
+	public static void main(String[] args) {
+   public class Dog{
+			 String name;
+			 String color;
+			int cost;
+			 void eat() {
+				 System.out.println("Dog is eating");
+				 void bark() {
+				 System.out.println("Dog is barking");
+					 void sleep() {
+						 System.out.println("Dog is sleping");
+						 class  Dog1{
+							 public static void main1 (String[]args) {
+							 Dog d1=new Dog();
+							 d1.eat();
+							 d1.bark();
+							 d1.sleep();
+							 Dog d2=new Dog();
+								d2.name="jacky";
+								d2.color="white";
+								d2.cost=25000 ;
+								System.out.println("name");
+								System.out.println("color");
+                       System.out.println("cost");
+							 
+						 }
+					 }
+				 }
+			 }
+		 }
+
+	}
+
+}
