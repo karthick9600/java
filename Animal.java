032a446322e9643bf226d5794fafbdaa7cd6b3f8
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-05-12 21:44:27 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,21 @@
+package java;
+
+class Main {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Main {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class Main {
+    public  static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();  // inherited method
+        d.bark(); // own method
+    }
+}
