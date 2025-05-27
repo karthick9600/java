e36c74811ccac120d67f6261321929b41ce29c12
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-05-27 21:52:32 +0530

    Add files via upload

---
+++ IntefaceDiamondExist.java
@@ -0,0 +1,49 @@
+package project6789;
+interface Calculator1{
+	void add();
+	void sub();
+}
+
+interface Calculator2{
+	void mul();
+	void div();
+}
+class Mycalculator implements Calculator1,Calculator2{
+	public void add(){
+		int a=200;
+		int b =100;
+		System.out.println(a+b);
+		
+	}
+	public void sub() {
+		int a=200;
+		int b=100;
+		System.out.println(a-b);
+	}
+	public void  mul() {
+		int  a=20;
+		int b=10;
+		
+		System.out.println(a*b);
+		
+	}
+	public void div() {
+		int a=20;
+		int b=10;
+		System.out.println(a/b);
+	}
+	
+}
+public class IntefaceDiamondExist {
+
+	public static void main(String[] args) {
+		Mycalculator mc =new Mycalculator();
+		mc.add();
+		mc.sub();
+		mc.mul();
+		mc.div();
+	
+
+	}
+
+}
