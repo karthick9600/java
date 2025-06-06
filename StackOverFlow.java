fe66a8e5fb3d51c5e9b6656480741e0960a0584a
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-06-06 21:48:18 +0530

    Add files via upload

---
+++ StackOverFlow.java
@@ -0,0 +1,18 @@
+package source;
+class Demos1{
+	void fun1() {
+		fun1();
+	}
+}
+
+public class StackOverFlow {
+
+	public static void main(String[] args) {
+		Demos1 d=new Demos1();
+		d.fun1();
+	
+		
+
+	}
+
+}
