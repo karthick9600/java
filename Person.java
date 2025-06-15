22a5993143c0a2b0117705c4a3de627c10357a7b
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-06-15 15:05:12 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,33 +1,29 @@
-package java;
+package one;
+
+import java.util.Comparator;
 
 public class Person {
-    private String name;
-    private int age;
+	private String name;
+	private int age;
+	public Person(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public  class PersonAgeComparator  implements Comparator<Person>{
+		public int compare(Person p1,Person p2) {
+			//compare person based on their age
+			return Integer.compare(p1.getAge(),p2.getAge());
+			
+		}
+		
 
-    public void setName(String newName) {
-        name = newName;
-    }
+	}
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int newAge) {
-        if (newAge > 0) {
-            age = newAge;
-        }
-    }
-
-    public static void main(String[] args) {
-        Person p = new Person();
-        p.setName("Karthick");
-        p.setAge(21);
-
-        System.out.println("Name: " + p.getName());  // Fix: Print name
-        System.out.println("Age: " + p.getAge());
-    }
 }
